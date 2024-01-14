29bccc0819f650dab150dc24f68499c3dc268713
ivantarbakou <c-ivan.tarbakou@integrate.com>
2024-01-14 22:14:04 +0200

    feat(refactor): improve error handling

--- api/api.go
+++ api/api.go
@@ -1,13 +1,29 @@
 package api
 
-// API is the API struct
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// API is the API struct.
 type API struct {
 	DBName string
 }
 
-// NewAPI returns a new API struct
+// NewAPI returns a new API struct.
 func NewAPI(db string) API {
 	return API{
 		DBName: db,
 	}
+}
+
+// ErrorResponse is the error response struct.
+type ErrorResponse struct {
+	Message string `json:"message"`
+}
+
+// sendErrorResponse sends an error response.
+func sendErrorResponse(w http.ResponseWriter, statusCode int, message string) {
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(ErrorResponse{Message: message})
 }
